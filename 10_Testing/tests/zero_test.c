be5daf3b513926844357696e00c4cf24e457ebcc
Artem Bakanov <s02190058@gse.cs.msu.ru>
2023-11-27 22:20:35 +0300

    10_Testing

---
+++ 10_Testing/tests/zero_test.c
@@ -0,0 +1,12 @@
+#include "test.h"
+
+int
+main(int argc, char **argv) {
+    float *a = 0;
+    buf_clear(a);
+    TEST("clear empty", buf_size(a) == 0);
+    TEST("clear no-op", a == 0);
+    return 0;
+}
+
+
